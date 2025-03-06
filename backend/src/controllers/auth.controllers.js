580c21a4c3fa8d5eb0fd7998ca49242147359404
kudhanshaik04 <kudhanshaik04@gmail.com>
2025-03-06 11:03:06 +0530

    setting up mongodb

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -1,16 +1,14 @@
-// Controller functions
 
 export const signup = (req, res) => {
-    // For now, just sending a simple message. Add actual signup logic here.
-    res.send("Sign up route hit - handle the signup logic here");
+   
+        const {fullName,email,pasword} = req.body;
+   
 };
 
-export const login = (req, res) => {
-    // Add login logic (e.g., check credentials)
+export const login = (req, res) => {    
     res.send("Login route hit - handle the login logic here");
 };
 
 export const logout = (req, res) => {
-    // Add logic to logout the user (e.g., destroy the session or token)
     res.send("Logout route hit - handle the logout logic here");
 };
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,18 @@
-import express from "express"
-import authRoutes from "./routes/auth.route.js"
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import dotenv from "dotenv";
+import {connectDB} from "./lib/db.js"
+
+dotenv.config(); // Load environment variables
 
 const app = express();
+const PORT = process.env.PORT || 3000; // Use 3000 as a fallback if PORT is not set
 
-app.use("/api/auth",authRoutes)
-app.listen(5001,()=>{
-    console.log("server is running on port 5001");
+app.use("/api/auth", authRoutes);
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log("Server is running on port:" + PORT);
+    connectDB();
 });
